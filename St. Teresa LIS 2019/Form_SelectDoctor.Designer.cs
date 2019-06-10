3dda32f69854630a4f4179e2c7bfdb68c87a1c52
AddwardLi <30455141@qq.com>
2019-06-10 22:37:33 +0800

    Add search function for the doctor page

--- St. Teresa LIS 2019/Form_SelectDoctor.Designer.cs
+++ St. Teresa LIS 2019/Form_SelectDoctor.Designer.cs
@@ -29,33 +29,23 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_SelectDoctor));
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.textBox_Serch_Doctor = new System.Windows.Forms.TextBox();
             this.label_Serch_Doctor = new System.Windows.Forms.Label();
             this.button_Cancel = new System.Windows.Forms.Button();
             this.button_OK = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 46);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(760, 353);
-            this.dataGridView1.TabIndex = 48;
             // 
             // textBox_Serch_Doctor
             // 
             this.textBox_Serch_Doctor.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.textBox_Serch_Doctor.ForeColor = System.Drawing.Color.Blue;
-            this.textBox_Serch_Doctor.Location = new System.Drawing.Point(260, 12);
+            this.textBox_Serch_Doctor.Location = new System.Drawing.Point(260, 11);
             this.textBox_Serch_Doctor.Name = "textBox_Serch_Doctor";
             this.textBox_Serch_Doctor.Size = new System.Drawing.Size(401, 27);
             this.textBox_Serch_Doctor.TabIndex = 47;
+            this.textBox_Serch_Doctor.TextChanged += new System.EventHandler(this.textBox_Serch_Doctor_TextChanged);
             // 
             // label_Serch_Doctor
             // 
@@ -63,7 +53,7 @@
             this.label_Serch_Doctor.BackColor = System.Drawing.Color.Transparent;
             this.label_Serch_Doctor.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.label_Serch_Doctor.ForeColor = System.Drawing.Color.Blue;
-            this.label_Serch_Doctor.Location = new System.Drawing.Point(115, 15);
+            this.label_Serch_Doctor.Location = new System.Drawing.Point(115, 14);
             this.label_Serch_Doctor.Name = "label_Serch_Doctor";
             this.label_Serch_Doctor.Size = new System.Drawing.Size(147, 22);
             this.label_Serch_Doctor.TabIndex = 46;
@@ -75,9 +65,9 @@
             this.button_Cancel.ForeColor = System.Drawing.Color.Red;
             this.button_Cancel.Image = ((System.Drawing.Image)(resources.GetObject("button_Cancel.Image")));
             this.button_Cancel.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button_Cancel.Location = new System.Drawing.Point(428, 416);
+            this.button_Cancel.Location = new System.Drawing.Point(428, 384);
             this.button_Cancel.Name = "button_Cancel";
-            this.button_Cancel.Size = new System.Drawing.Size(119, 44);
+            this.button_Cancel.Size = new System.Drawing.Size(119, 41);
             this.button_Cancel.TabIndex = 64;
             this.button_Cancel.Text = "Cancel";
             this.button_Cancel.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -90,28 +80,41 @@
             this.button_OK.ForeColor = System.Drawing.Color.Blue;
             this.button_OK.Image = ((System.Drawing.Image)(resources.GetObject("button_OK.Image")));
             this.button_OK.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.button_OK.Location = new System.Drawing.Point(236, 416);
+            this.button_OK.Location = new System.Drawing.Point(236, 384);
             this.button_OK.Name = "button_OK";
-            this.button_OK.Size = new System.Drawing.Size(119, 44);
+            this.button_OK.Size = new System.Drawing.Size(119, 41);
             this.button_OK.TabIndex = 63;
             this.button_OK.Text = "OK";
             this.button_OK.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.button_OK.UseVisualStyleBackColor = true;
+            this.button_OK.Click += new System.EventHandler(this.button_OK_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(760, 326);
+            this.dataGridView1.TabIndex = 68;
             // 
             // Form_SelectDoctor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(220)))), ((int)(((byte)(179)))));
-            this.ClientSize = new System.Drawing.Size(784, 472);
+            this.ClientSize = new System.Drawing.Size(784, 436);
             this.ControlBox = false;
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button_Cancel);
             this.Controls.Add(this.button_OK);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox_Serch_Doctor);
             this.Controls.Add(this.label_Serch_Doctor);
             this.Name = "Form_SelectDoctor";
             this.Text = "Select Doctor";
+            this.Load += new System.EventHandler(this.Form_SelectDoctor_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -119,11 +122,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TextBox textBox_Serch_Doctor;
         private System.Windows.Forms.Label label_Serch_Doctor;
         private System.Windows.Forms.Button button_Cancel;
         private System.Windows.Forms.Button button_OK;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- St. Teresa LIS 2019/Form_SelectDoctor.cs
+++ St. Teresa LIS 2019/Form_SelectDoctor.cs
@@ -11,6 +11,12 @@ namespace St.Teresa_LIS_2019
 {
     public partial class Form_SelectDoctor : Form
     {
+        private DataTable dt;
+        private DataSet doctorDataSet = new DataSet();
+
+        public delegate void DoctorSelectedMore(string idStr);
+        public DoctorSelectedMore OnDoctorSelectedMore;
+
         public Form_SelectDoctor()
         {
             InitializeComponent();
@@ -19,6 +25,144 @@ namespace St.Teresa_LIS_2019
         private void button_Cancel_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Enter)
+            {
+                if (textBox_Serch_Doctor.Focused)
+                {
+                    string sql = string.Format("SELECT doctor,cname,initial,address1,tel1,fax,opd,contact,id FROM [DOCTOR] WHERE DOCTOR LIKE '%{0}%' OR CNAME LIKE '%{0}%'", textBox_Serch_Doctor.Text.Trim());
+                    DBConn.fetchDataIntoDataSetSelectOnly(sql, doctorDataSet, "doctor");
+
+                    DataTable dt = new DataTable();
+                    dt.Columns.Add("Select", typeof(bool));
+                    dt.Columns.Add("Doctor's Name");
+                    dt.Columns.Add("Chinese Name");
+                    dt.Columns.Add("Initial");
+                    dt.Columns.Add("Address1");
+                    dt.Columns.Add("Tel1");
+                    dt.Columns.Add("Fax");
+                    dt.Columns.Add("Opd");
+                    dt.Columns.Add("Contact");
+                    dt.Columns.Add("Id");
+
+                    foreach (DataRow mDr in doctorDataSet.Tables["doctor"].Rows)
+                    {
+                        dt.Rows.Add(new object[] { "false", mDr["doctor"], mDr["cname"], mDr["initial"], mDr["address1"], mDr["tel1"], mDr["fax"], mDr["opd"], mDr["contact"], mDr["id"] });
+                    }
+
+                    dataGridView1.DataSource = dt;
+                }
+                else
+                {
+                    button_OK.PerformClick();
+                }
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void textBox_Serch_Doctor_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button_OK_Click(object sender, EventArgs e)
+        {
+            string idStr = "";
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if (bool.Parse(dataGridView1.Rows[i].Cells[0].Value.ToString()) == true)
+                {
+                    if (dataGridView1.Rows[i].Cells[9].Value.ToString() != "")
+                    {
+                        if (idStr == "")
+                        {
+                            idStr += dataGridView1.Rows[i].Cells[9].Value.ToString();
+                        }
+                        else
+                        {
+                            idStr += "," + dataGridView1.Rows[i].Cells[9].Value.ToString();
+                        }
+
+                    }
+                }
+            }
+            OnDoctorSelectedMore(idStr);
+            this.Close();
+        }
+
+        private void Form_SelectDoctor_Load(object sender, EventArgs e)
+        {
+            loadDataGridViewDate();
+            dataGridViewFormat();
+        }
+
+        private void loadDataGridViewDate()
+        {
+            string sql = "SELECT doctor,cname,initial,address1,tel1,fax,opd,contact,id FROM [DOCTOR]";
+            DBConn.fetchDataIntoDataSetSelectOnly(sql, doctorDataSet, "doctor");
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Select", typeof(bool));
+            dt.Columns.Add("Doctor's Name");
+            dt.Columns.Add("Chinese Name");
+            dt.Columns.Add("Initial");
+            dt.Columns.Add("Address1");
+            dt.Columns.Add("Tel1");
+            dt.Columns.Add("Fax");
+            dt.Columns.Add("Opd");
+            dt.Columns.Add("Contact");
+            dt.Columns.Add("Id");
+
+            foreach (DataRow mDr in doctorDataSet.Tables["doctor"].Rows)
+            {
+                dt.Rows.Add(new object[] { "false", mDr["doctor"], mDr["cname"], mDr["initial"], mDr["address1"], mDr["tel1"], mDr["fax"], mDr["opd"], mDr["contact"], mDr["id"] });
+            }
+
+            dataGridView1.DataSource = dt;
+        }
+        private void dataGridViewFormat()
+        {
+            DataGridViewColumn column0 = dataGridView1.Columns[0];
+            column0.Width = 30;
+            DataGridViewColumn column1 = dataGridView1.Columns[1];
+            column1.Width = 190;
+            column1.ReadOnly = true;
+            DataGridViewColumn column2 = dataGridView1.Columns[2];
+            column2.Width = 145;
+            column2.ReadOnly = true;
+            DataGridViewColumn column3 = dataGridView1.Columns[3];
+            column3.Width = 130;
+            column3.ReadOnly = true;
+            DataGridViewColumn column4 = dataGridView1.Columns[4];
+            column4.Width = 30;
+            column4.ReadOnly = true;
+            DataGridViewColumn column5 = dataGridView1.Columns[5];
+            column5.Width = 30;
+            column5.ReadOnly = true;
+            DataGridViewColumn column6 = dataGridView1.Columns[6];
+            column6.Width = 60;
+            column6.ReadOnly = true;
+            DataGridViewColumn column7 = dataGridView1.Columns[7];
+            column7.Width = 50;
+            column7.ReadOnly = true;
+            DataGridViewColumn column8 = dataGridView1.Columns[8];
+            column8.Width = 60;
+            column8.ReadOnly = true;
+            DataGridViewColumn column9 = dataGridView1.Columns[9];
+            column9.Width = 1;
+            column9.ReadOnly = true;
+            this.dataGridView1.DefaultCellStyle.Font = new Font("Microsoft Sans Serif", 10, FontStyle.Bold);
+
+            dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font("Microsoft Sans Serif", 11, FontStyle.Bold);
+            dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.Blue;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.TopLeft;
+
+            dataGridView1.EnableHeadersVisualStyles = false;
+
         }
     }
 }
