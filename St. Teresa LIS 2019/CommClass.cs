6b352557600c9c07c53392a57f935e752bfd74a8
AddwardLi <30455141@qq.com>
2019-07-04 23:46:30 +0800

    Implement the login function

--- St. Teresa LIS 2019/CommClass.cs
+++ St. Teresa LIS 2019/CommClass.cs
@@ -132,4 +132,10 @@ namespace St.Teresa_LIS_2019
             }
         }
     }
+
+    public static class CurrentUser
+    {
+        public static int currentUserLevel;
+        public static int userId;
+    }
 }
--- St. Teresa LIS 2019/Form_Login.cs
+++ St. Teresa LIS 2019/Form_Login.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace St.Teresa_LIS_2019
 {
@@ -23,26 +25,58 @@ namespace St.Teresa_LIS_2019
 
         private void FromLogin_Load(object sender, EventArgs e)
         {
-            textBox_Password.KeyUp += new KeyEventHandler(textBox_Password_KeyUp);
-            textBox_UserID.KeyUp += new KeyEventHandler(textBox_UserID_KeyUp);
+            //textBox_Password.KeyUp += new KeyEventHandler(textBox_Password_KeyUp);
+            //textBox_UserID.KeyUp += new KeyEventHandler(textBox_UserID_KeyUp);
         }
 
         private void textBox_Password_KeyUp(object sender, KeyEventArgs e)
         {
-            if (textBox_Password.Text == "sys" && textBox_UserID.Text == "SYS")
-            {
-                if (e.KeyCode == Keys.Enter)
-                {
-                    Form_MainMenu open = new Form_MainMenu();
-                    open.Show();
-                    this.Hide();
-                }
-            }
-
+            
         }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Enter)
+            {
+                if (textBox_Password.Focused || textBox_UserID.Focused)
+                {
+                    if (textBox_Password.Text != "" && textBox_UserID.Text != "")
+                    {
+                        SqlConnection conn = DBConn.getConnection();
+                        DataSet userDataSet = new DataSet();
+                        string sql = string.Format("select * FROM [USER] where user_id = '{0}' and (password='{1}' OR (password2 is not null and password2='{1}' and level > 0))", textBox_UserID.Text.Trim(), textBox_Password.Text.Trim());
+                        DBConn.fetchDataIntoDataSetSelectOnly(sql, userDataSet, "USER");
+
+                        if (userDataSet.Tables["USER"].Rows.Count > 0)
+                        {
+
+                            DataRow mDr = userDataSet.Tables["USER"].Rows[0];
+
+                            CurrentUser.currentUserLevel = int.Parse(mDr["LEVEL"].ToString());
+                            CurrentUser.userId = int.Parse(mDr["id"].ToString());
+                            Form_MainMenu open = new Form_MainMenu();
+                            open.Show();
+                            this.Hide();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Invalid user name or password");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter the user name and password");
+                    }
+                }
+
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
         private void textBox_UserID_KeyUp(object sender, KeyEventArgs e)
         {
-            if (textBox_Password.Text == "sys" && textBox_UserID.Text == "SYS")
+            /*if (textBox_Password.Text == "sys" && textBox_UserID.Text == "SYS")
             {
                 if (e.KeyCode == Keys.Enter)
                 {
@@ -50,7 +84,7 @@ namespace St.Teresa_LIS_2019
                     open.Show();
                     this.Hide();
                 }
-            }
+            }*/
 
         }
 
