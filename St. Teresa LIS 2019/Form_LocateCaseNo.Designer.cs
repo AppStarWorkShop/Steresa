072425f0a39725b1ffce88848e32cf4c032db224
AddwardLi <30455141@qq.com>
2019-08-07 23:07:19 +0800

    Fix the bug for the ebv and bx/cy search page

--- St. Teresa LIS 2019/Form_LocateCaseNo.Designer.cs
+++ St. Teresa LIS 2019/Form_LocateCaseNo.Designer.cs
@@ -143,8 +143,10 @@
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 83);
+            this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(984, 595);
             this.dataGridView1.TabIndex = 79;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
